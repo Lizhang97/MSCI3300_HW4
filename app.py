1ab66e526dab571cec42381b035de41d24049f52
Xinwen Zhang <xinwen-zhang@uiowa.edu>
2020-04-11 21:44:23 -0500

    Modification

--- app.py
+++ app.py
@@ -6,7 +6,13 @@ from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import or_
 import pymysql
-import secrets
+#import secrets
+import os
+
+dbuser=os.environ.get('DBUSER')
+dbpass=os.environ.get('DBPASS')
+dbhost=os.environ.get('DBHOST')
+dbname=os.environ.get('DBNAME')
 
 conn = "mysql+pymysql://{0}:{1}@{2}/{3}".format(secrets.dbuser, secrets.dbpass, secrets.dbhost, secrets.dbname)
 
